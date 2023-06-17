343c8c38f0aa2c4352a1435403a926b6980cecbd
Expo <crdailey01@gmail.com>
2023-06-16 22:22:44 -0700

    do it

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.tree.TreeNode;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+
+    }
+}

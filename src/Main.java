bbfb32669e1abe42bdac1c553933a63f28674332
Expo <crdailey01@gmail.com>
2023-06-17 21:39:36 -0700

    one concern left

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,7 @@ class Solution {
         if(root == null) return true;
         if(Math.abs(height(root.left) - height(root.right)) >1) return false;
         return isBalanced(root.left) && isBalanced(root.right);
-        //can't just return true but i'm not sure why if the subtrees are not > 1 in height?
+        // can't just return true but i'm not sure why if the subtrees are not > 1 in height?
     }
 
     public int height(TreeNode node) {
